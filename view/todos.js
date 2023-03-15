e6172e60abd6ab8cb9ccaf9ff380351b3d6d98a2
Lee Dae Kyoung <coolbino@naver.com>
2023-03-15 22:35:35 +0900

    기존의 Todo는 string 으로 DOM 을 그리고있어 바인딩이 어려움, DOM을 스탬프 형식으로 생성후 이벤트 바인딩 할수 있는 조건 만들어주기

--- view/todos.js
+++ view/todos.js
@@ -1,18 +1,39 @@
-const getTodoElement = ({
-                            text,
-                            completed
-                        }) => `
-  <li ${completed ? 'class="completed"' : ''}>
-    <div class="view">
-      <input 
-        ${completed ? 'checked' : ''}
-        class="toggle" 
-        type="checkbox">
-      <label>${text}</label>
-      <button class="destroy"></button>
-    </div>
-    <input class="edit" value="${text}">
-  </li>`;
+let template;
+
+const createNewTodoNode = () => {
+    if (!template) {
+        template = document.getElementById('todo-item')
+    }
+
+    return template
+        .content
+        .firstElementChild
+        .cloneNode(true)
+}
+
+const getTodoElement = todo => {
+    const {
+        text,
+        completed
+    } = todo
+
+    const element = createNewTodoNode()
+
+    element.querySelector('input.edit').value = text
+    element.querySelector('label').textContent = text
+
+    if (completed) {
+        element
+            .classList
+            .add('completed')
+
+        element
+            .querySelector('input.toggle')
+            .checked = true
+    }
+
+    return element
+}
 
 export default (targetElement, {todos}) => {
     const newTodoList = targetElement.cloneNode(true);
